49cb6c9ce0d96851153028d631a017706310801b
Elanna Grossman <elanna.grossman@gmail.com>
2015-09-10 19:53:21 -0700

    Initial commit

---
+++ GenericsTest.java
@@ -0,0 +1,51 @@
+/*
+ Elanna Grossman
+ Application that uses an abstract method to interact with ArrayList of Strings.
+ */
+
+//package program;
+
+import java.util.ArrayList;
+
+public class GenericsTest
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("two");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+
+        //Remove duplicates from ArrayList of Strings
+        ArrayList<String> unique = removeDuplicates(list);
+
+        for (String element : unique)
+        {
+            System.out.println(element);
+        }//end for
+    }//end main
+
+    private static <E> ArrayList<E> removeDuplicates(ArrayList<E> list)
+    {
+        //Store unique items in result
+        ArrayList<E> result = new ArrayList<>();
+
+        // Record encountered Strings.
+        ArrayList<E> set = new ArrayList<>();
+
+        for (E item : list)
+        {
+            //If String is not in set, add it to the list and the set
+            if (!set.contains(item))
+            {
+                result.add(item);
+                set.add(item);
+            }//end if
+        }//end for
+        return result;
+    }//end removeDuplicates
+}//end GenericsTest
